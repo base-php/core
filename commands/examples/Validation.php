25b681cb1e25a23b660be29bff48062c8ff4ef84
Nisa Delgado <nisadelgado@gmail.com>
2021-06-08 22:28:46 -0400

    Archivo de ejemplo de validaciones corregido

--- commands/examples/Validation.php
+++ commands/examples/Validation.php
@@ -1,1 +1,26 @@
-Validation.php
+<?php
+
+namespace App\Validations;
+
+class UserStore extends Validation
+{
+	/**
+	* Get the validation rules that apply to the request.
+	*
+	* @return array
+	*/
+	public function rules()
+	{
+		return [];
+	}
+
+	/**
+	 * Get the error messages for the defined validation rules.
+	 *
+	 * @return array
+	 */
+	public function messages()
+	{
+		return [];
+	}
+}
