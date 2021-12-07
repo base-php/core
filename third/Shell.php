439d123223f157ff36de987023aa2215094f558e
Nisa Delgado <nisadelgado@gmail.com>
2021-12-07 13:33:16 -0400

    Quitado aviso de nuevo versión de psy/psysh

--- third/Shell.php
+++ third/Shell.php
@@ -1518,7 +1518,7 @@ class Shell extends Application
         try {
             $client = $this->config->getChecker();
             if (!$client->isLatest()) {
-                $this->output->writeln(\sprintf('New version is available (current: %s, latest: %s)', self::VERSION, $client->getLatest()));
+                
             }
         } catch (\InvalidArgumentException $e) {
             $this->output->writeln($e->getMessage());
