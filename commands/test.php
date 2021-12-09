ee9d0161813defc190bdce5a8b04eb95fd10406d
Nisa Delgado <nisadelgado@gmail.com>
2021-12-09 08:32:17 -0400

    Comando ejecuta uno o todos los test

--- commands/test.php
+++ commands/test.php
@@ -1,6 +1,7 @@
 <?php
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 
 class Test extends Command
 {
@@ -8,9 +9,21 @@ class Test extends Command
 
     protected static $defaultDescription = 'Run unit tests';
 
+    public function configure()
+    {
+        $this->addArgument('test', InputArgument::OPTIONAL);
+    }
+
     protected function execute($input, $output)
     {
-        system('vendor\bin\phpunit');
+        $test = $input->getArgument('test');
+
+        if ($test) {
+            system('vendor\bin\phpunit ' . $test);
+        } else {
+            system('vendor\bin\phpunit');
+        }
+
         return Command::SUCCESS;
     }
 }
