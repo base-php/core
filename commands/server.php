c05e68e5724d1a3cf6e4654f236be3c00bede7be
Nisa Delgado <nisadelgado@gmail.com>
2022-01-04 08:18:58 -0400

    Comando para levantar servidor de desarrollo admite parametro de puerto

--- commands/server.php
+++ commands/server.php
@@ -1,16 +1,23 @@
 <?php
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 
 class Server extends Command
 {
     protected static $defaultName = 'server';
 
-    protected static $defaultDescription = 'Build development server on port 8080';
+    protected static $defaultDescription = 'Build development server';
+
+    public function configure()
+    {
+        $this->addArgument('port', InputArgument::OPTIONAL);
+    }
 
     protected function execute($input, $output)
     {
-        system('php -S localhost:8080');
+        $port = ($input->getArgument('port')) ? $input->getArgument('port') : 8080;
+        system('php -S localhost:' . $port);
         return Command::SUCCESS;
     }
 }
