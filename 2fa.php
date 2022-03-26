f1011dba1f9259a8fa7088ffad70d8a102bdbf3c
Nisa Delgado <nisadelgado@gmail.com>
2022-03-26 00:17:35 -0400

    Clase para autenticación de dos factores

---
+++ 2fa.php
@@ -0,0 +1,41 @@
+<?php
+
+use Endroid\QrCode\QrCode;
+use PragmaRX\Google2FA\Google2FA;
+
+class TwoFA
+{
+    // endroid/qr-code
+    // pragmarx/google2fa-qrcode
+
+    public $instance;
+
+    public $key;
+
+    public function __construct()
+    {
+        $this->instance = new Google2FA();
+        $this->key      = auth()->2fa;
+    }
+
+    public function verify($code)
+    {
+        return $this->instace->verifyKey($this->key, $code);
+    }
+
+    public function qr($size)
+    {
+        $value = $this->instance->getQRCodeUrl(
+            config('application_name'),
+            '',
+            $this->key
+        );
+
+        $qr = new QrCode($value);
+        $qr->setSize($size);
+
+        $url = $qr->writeDataUri();
+
+        return $url;
+    }
+}
