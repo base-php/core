0779d4060ac9c656c31e845e48f0040eabf457f9
Nisa Delgado <nisadelgado@gmail.com>
2022-04-18 09:32:57 -0400

    Notificaciones P3

--- notification.php
+++ notification.php
@@ -2,10 +2,36 @@
 
 namespace App\Models\Notifications;
 
+use App\Models\Notification as NotificationModel;
+
 class Notification
 {
 	public function send($users)
 	{
 		$users = is_iterable($users) ? $users : collect($users);
+
+		foreach ($users as $user) {
+			if (in_array('database', $this->via())) {
+				$this->database($user, $this->array());
+			}
+		}		
+	}
+
+	public function database($user, $array)
+	{
+		$type = get_class($this);
+
+		$data = [
+			'id_user' => $user->id
+		];
+
+		$data = array_push($data, $array);
+
+		NotificationModel::create([
+			'type'			=> $type,
+			'data'			=> $data,
+			'date_create'	=> now('Y-m-d H:i:s'),
+			'date_update'	=> now('Y-m-d H:i:s')
+		]);
 	}
 }
