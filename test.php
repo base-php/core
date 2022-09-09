bea1574b7e19ff6eba32ce00b568b4cea83e9567
Nisa Delgado <nisadelgado@gmail.com>
2022-09-09 08:19:44 -0400

    Método get para pruebas

--- test.php
+++ test.php
@@ -6,5 +6,10 @@ use PHPUnit\Framework\TestCase;
 
 class Test extends TestCase
 {
-
+	public function get($route)
+	{
+		$url = "localhost:8080/$route";
+		$request = http()->get($url);
+		return $request;
+	}
 }
