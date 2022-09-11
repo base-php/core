ef84a83de1d97b9862d56f1d04171251fdc97710
Nisa Delgado <nisadelgado@gmail.com>
2022-09-11 13:29:55 -0400

    Método para pruebas a base de datos

--- test.php
+++ test.php
@@ -2,10 +2,26 @@
 
 namespace Tests;
 
+use DB;
 use PHPUnit\Framework\TestCase;
 
 class Test extends TestCase
 {
+	public function assertDatabaseHas($table, $data)
+	{
+		include 'database.php';
+
+		$db = DB::table($table)
+			->where($data)
+			->get();
+
+		if ($db->count()) {
+			return $this->assertTrue(true);
+		}
+
+		return $this->fail('Valor no existe en base de datos');
+	}
+
 	public function get($route)
 	{
 		$url = "localhost:8080/$route";
