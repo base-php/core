bd225c9a694cf41c8c7959c115d7842928dc05f9
Nisa Delgado <nisadelgado@gmail.com>
2022-11-11 08:59:02 -0400

    Modelo rol corregido

--- permission/models/Role.php
+++ permission/models/Role.php
@@ -22,8 +22,8 @@ class Role extends Model
                 ->insertUsing(
                     ['id_role', 'id_permission'],
                     
-                    DB::table('permission')
-                        ->select($id_role, 'id')
+                    DB::table('permissions')
+                        ->selectRaw("$id_role, id")
                         ->where('name', $permission)
                 );
         }
@@ -33,7 +33,7 @@ class Role extends Model
     {
         $permissions = DB::table('permissions')
             ->select('permissions.id', 'permissions.name', 'permissions.description')
-            ->leftJoin('role_has_permissions', 'permissions.id = role_has_permissions.id_role')
+            ->leftJoin('role_has_permissions', 'permissions.id', '=', 'role_has_permissions.id_role')
             ->where('role_has_permissions.id_role', $this->id)
             ->get();
 
@@ -46,7 +46,7 @@ class Role extends Model
 
         foreach ($permissions as $permission) {
             DB::table('role_has_permissions')
-                ->whereExists(function ($query) {
+                ->whereExists(function ($query) use ($permission) {
                     $query->select(DB::raw(1))
                         ->from('permissions')
                         ->where('name', $permission);
@@ -57,7 +57,7 @@ class Role extends Model
 
     public function syncPermissions($permissions)
     {
-        $id_role = $this->id_role;
+        $id_role = $this->id;
 
         DB::table('role_has_permissions')->where('id_role', $id_role)->delete();
 
@@ -66,8 +66,8 @@ class Role extends Model
                 ->insertUsing(
                     ['id_role', 'id_permission'],
 
-                    DB::table('permission')
-                        ->select($id_role, 'id')
+                    DB::table('permissions')
+                        ->selectRaw("$id_role, id")
                         ->where('name', $permission)
                 );
         }
