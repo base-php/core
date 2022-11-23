0d105a0ac462dd881b1795b6a1338ceec21a2f75
Nisa Delgado <nisadelgado@gmail.com>
2022-11-23 09:56:08 -0400

    Reorganizado de archivos para archivos lang

--- helpers.php
+++ helpers.php
@@ -112,6 +112,20 @@ function http()
     return new HttpClient;
 }
 
+function lang($key)
+{
+    try {
+        $array = explode('.', $key);
+
+        $file = $array[0];
+        $key = $array[1];
+
+        return $_ENV['translate'][$file][$key];
+    } catch (Exception $exception) {
+        return $key;
+    }
+}
+
 function logs()
 {
     return DB::table('logs');
