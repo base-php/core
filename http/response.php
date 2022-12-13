1f79948a87a23723983958b03707fefbd6685be8
Nisa Delgado <nisadelgado@gmail.com>
2022-12-13 08:53:47 -0400

    Headers en response

--- http/response.php
+++ http/response.php
@@ -2,10 +2,19 @@
 
 class Response
 {
+	public function headers($headers)
+	{
+		foreach ($headers as $key => $value) {
+			header($key . ': ' . $value);
+		}
+
+		return $this;
+	}
+
 	public function json($iterable, $status_code = 200)
 	{
 		http_response_code($status_code);
 		header('Content-Type: application/json; charset=utf-8');
-		echo json_encode($iterable);
+		return json_encode($iterable);
 	}
 }
