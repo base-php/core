b320681dd52c9ffdd8f4b557fe0eb18377f1f360
Nisa Delgado <nisadelgado@gmail.com>
2022-12-20 08:42:31 -0400

    Middleware para permisos

---
+++ permissions/middleware/Can.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Middleware;
+
+class Can
+{
+	public function handle($request, $next, $permission)
+	{
+		if (!can($permission)) {
+			return abort(401);
+		}
+
+		return $next($request);
+	}
+}
