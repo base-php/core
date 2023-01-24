4192262b8865c2507b411200a45cab403b8f02df
Nisa Delgado <nisadelgado@gmail.com>
2023-01-24 08:58:34 -0400

    Trait CRUD

---
+++ crud/traits/crud.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controllers;
+
+trait CRUD
+{
+	public $buttons;
+	public $class;
+	public $css;
+	public $items;
+	public $style;
+	public $widgets;
+
+	public function resolve($method, $argument = null)
+	{
+		$this->$method();
+
+		$data['fields'] = (object) $this->fields;
+		$data['items'] = $this->items;
+
+		$data['class'] = isset($this->class) ? (object) $this->class : (object) [];
+		$data['style'] = isset($this->style) ? (object) $this->style : (object) [];
+
+		$data['css'] = isset($this->css) ? (object) $this->css : (object) [];
+		$data['js'] = isset($this->js) ? (object) $this->js : (object) [];
+
+		$data['buttons'] = isset($this->buttons) ? (object) $this->buttons : (object) [];
+
+		$data['widgets'] = isset($this->widgets) ? (object) $this->widgets : (object) [];
+
+		$data['paginationView'] = $paginationView ?? 'default';
+
+		return view('crud:' . $method, $data);
+	}
+}
