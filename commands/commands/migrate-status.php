9777c079db41d2d62bce0e3a7f84c2b834e2bc0b
Nisa Delgado <nisadelgado@gmail.com>
2023-03-29 07:38:26 -0400

    Migrate status database, path

--- commands/commands/migrate-status.php
+++ commands/commands/migrate-status.php
@@ -10,13 +10,30 @@ class MigrateStatus extends Command
 
     protected static $defaultDescription = 'Muestra el estado de cada migración';
 
+    public function configure()
+    {
+        $this->addOption('database', null, InputOption::VALUE_OPTIONAL, 'Conexión de base de datos a utilizar', 'default');
+        $this->addOption('path', null, InputOption::VALUE_REQUIRED, 'Ruta al archivo de migración que se ejecutará');
+    }
+
     protected function execute($input, $output)
     {
         $style = new SymfonyStyle($input, $output);
 
+        $connection = $input->getOption('database') ?? 'default';
+        $path = $input->getOption('path');
+
         foreach (scandir('database') as $item) {
             if (! is_dir($item)) {
                 $class = require 'database/' . $item;
+
+                if ($class->connection != $connection) {
+                    return;
+                }
+
+                if ($path && $path != 'database/' . $item) {
+                    return;
+                }
 
                 $name = str_replace('.php', '', $item);
 
