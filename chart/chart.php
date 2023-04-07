a7c36b1a50bc7e3489edb3b262369e4f646a3e80
Nisa Delgado <nisadelgado@gmail.com>
2023-04-07 00:30:55 -0400

    script, setScriptAttribute en Chart

--- chart/chart.php
+++ chart/chart.php
@@ -5,13 +5,14 @@ class Chart
 	public $dataset = [];
 	public $labels;
 	public $options;
+	public $scriptAttribute = '';
+	public $type;
 
-	public function dataset($name, $type, $data)
+	public function dataset($name, $data)
 	{
 		$count = count($this->dataset);
 
 		$this->dataset[$count]['name'] = $name;
-		$this->dataset[$count]['type'] = $type;
 		$this->dataset[$count]['data'] = $data;
 
 		return $this;
@@ -26,6 +27,42 @@ class Chart
 	public function options($options)
 	{
 		$this->options = $options;
+		return $this;
+	}
+
+	public function script()
+	{
+		if ($this->type == 'chartjs') {
+			echo '<script ' . $this->scriptAttribute . ' src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.2.1/chart.min.js"></script>';
+		}
+
+		if ($this->type == 'highcharts') {
+			echo '<script ' . $this->scriptAttribute . ' src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/10.3.3/highcharts.js"></script>';
+			echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highcharts/10.3.3/css/highcharts.min.css">';
+		}
+
+		if ($this->type == 'fusioncharts') {
+			echo '<script ' . $this->scriptAttribute . ' src="https://cdnjs.cloudflare.com/ajax/libs/fusioncharts/3.20.0/fusioncharts.js"></script>';
+		}
+
+		if ($this->type == 'echarts') {
+			echo '<script ' . $this->scriptAttribute . ' src="https://cdnjs.cloudflare.com/ajax/libs/echarts/5.4.2/echarts.min.js"></script>';
+		}
+
+		if ($this->type == 'c3') {
+			echo '<script ' . $this->scriptAttribute . ' src="https://cdnjs.cloudflare.com/ajax/libs/c3/0.7.20/c3.min.js"></script>';
+			echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.7.20/c3.css">';
+		}
+	}
+
+	public function setScriptAttribute($scriptAttribute)
+	{
+		$keys = array_keys($scriptAttribute);
+		$values = array_values($scriptAttribute);
+
+		$scriptAttribute = $keys[0] . '="' . $values[0] . '"';
+
+		$this->scriptAttribute = $scriptAttribute;
 		return $this;
 	}
 }
