128542d258d981c3b9ffd63e4bef74b8ccc66533
Nisa Delgado <nisadelgado@gmail.com>
2023-05-13 12:59:15 -0400

    Autenticación con Bootstrap

--- auth/views/bootstrap/auth/forgot-password.blade.php
+++ auth/views/bootstrap/auth/forgot-password.blade.php
@@ -1,21 +1,28 @@
 <x-template-auth>
     <x-alert/>
 
-    <div>
-        <form class="flex flex-col" method="POST" action="/forgot-password">
-            <div>
-                <p class="text-justify">{{ lang('auth.text') }}</p>
-            </div>
+    <div class="row">
+        <div class="col-md-5 mx-auto">
+            <div class="card">
+                <div class="card-body">
+                    <form method="POST" action="/forgot-password">
+                        <div>
+                            <p class="text-justify">{{ lang('auth.text') }}</p>
+                        </div>
 
-            <div class="mt-3 mb-5">
-                <x-label for="email" text="{{ lang('auth.email') }}"/>
-                <x-input name="email" required type="email"/>
-            </div>
+                        <div class="mt-3 mb-5">
+                            <x-label for="email" text="{{ lang('auth.email') }}"/>
+                            <x-input name="email" required type="email"/>
+                        </div>
 
-            <x-button color="black">
-                <i class="fas fa-link mr-2"></i>
-                {{ lang('auth.send_link') }}
-            </x-button>
-        </form>
+                        <x-button color="dark" border="dark">
+                            <i class="fas fa-link mr-2"></i>
+                            {{ lang('auth.send_link') }}
+                        </x-button>
+                    </form>
+                </div>
+            </div>            
+        </div>
     </div>
+
 </x-template-auth>
