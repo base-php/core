483eb6fb82ab4bb724be69cd678914e6be414535
Nisa Delgado <nisadelgado@gmail.com>
2023-05-15 08:20:49 -0400

    Autenticación con Bootstrap

--- auth/views/bootstrap/auth/2fa.blade.php
+++ auth/views/bootstrap/auth/2fa.blade.php
@@ -1,22 +1,28 @@
 <x-template-auth>
-    <div>
-        <x-alert/>
+    <div class="row">
+        <div class="col-4 mx-auto">
+            <div class="card">
+                <div class="card-body">
+                    <x-alert/>
 
-        <form class="flex flex-col" method="POST" action="{{ '/2fa/' . $id }}">
-            <input type="hidden" name="id" value="{{ $id }}">
-            <input type="hidden" name="redirect" value="{{ get('redirect') }}">
+                    <form class="flex flex-col" method="POST" action="{{ '/2fa/' . $id }}">
+                        <input type="hidden" name="id" value="{{ $id }}">
+                        <input type="hidden" name="redirect" value="{{ get('redirect') }}">
 
-            <p>Confirme el acceso a su cuenta ingresando el código de autenticación proporcionado por su aplicación de autenticación.</p>
+                        <p>Confirme el acceso a su cuenta ingresando el código de autenticación proporcionado por su aplicación de autenticación.</p>
 
-            <div class="mt-3 mb-5">
-                <x-label for="code" text="{{ lang('auth.code') }}"/>
-                <x-input name="code" required/>
+                        <div class="mt-3 mb-3">
+                            <x-label for="code" text="{{ lang('auth.code') }}"/>
+                            <x-input name="code" required/>
+                        </div>
+
+                        <x-button color="dark" border="dark">
+                            <i class="fas fa-sign-in-alt mr-2"></i>
+                            {{ lang('auth.verify') }}
+                        </x-button>
+                    </form>
+                </div>
             </div>
-
-            <x-button color="black">
-                <i class="fas fa-sign-in-alt mr-2"></i>
-                {{ lang('auth.verify') }}
-            </x-button>
-        </form>
+        </div>
     </div>
 </x-template-auth>
