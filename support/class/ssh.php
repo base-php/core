3339e1950b1b9a08c14e365ded519233f74f70a6
Nisa Delgado <nisadelgado@gmail.com>
2023-05-17 05:54:40 -0400

    Exec, success y timeout en SSH

--- support/class/ssh.php
+++ support/class/ssh.php
@@ -13,15 +13,31 @@ class SSH
         }
     }
 
+    public function exec($command)
+    {
+        $this->instance->execute($command);
+        return $this;
+    }
+
     public function login($user, $pass, $port = 22)
     {
     	$this->instance = SpatieSsh::create($user, $pass, $port);
     	return $this;
     }
 
-    public function exec($command)
+    public function output()
     {
-    	$this->instance->execute($command);
-    	return $this;
+        return $this->instance->getOutput();
+    }
+
+    public function success()
+    {
+        return $this->instance->isSuccessful();
+    }
+
+    public function timeout($timeout)
+    {
+        $this->instance->setTimeout($timeout);
+        return $this;
     }
 }
