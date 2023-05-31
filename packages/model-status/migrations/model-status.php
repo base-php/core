af090a0fed8068a60c1eb645917950f70b62139d
Nisa Delgado <nisadelgado@gmail.com>
2023-05-31 00:50:02 -0400

    Migración para estados en modelos

---
+++ packages/model-status/migrations/model-status.php
@@ -0,0 +1,41 @@
+<?php
+
+return new class extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up(): void
+	{
+		$this->schema->create('model_status', function ($table) {
+            $table->id();
+
+            $table->string('name');
+            $table->text('reason')->nullable();
+
+            $table->string('model');
+            $table->int('id_model');
+
+            $table->datetime('date_create')
+                ->useCurrent();
+
+            $table->datetime('date_update')
+                ->useCurrent()
+                ->setCurrentOnUpdate();
+
+            $table->unique(['name', 'scope']);
+        });
+	}
+
+	/**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        $this->schema->dropIfExists('features');
+    }
+};
