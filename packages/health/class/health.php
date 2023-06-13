28b3bb4c64d30652500005a2d81c3b308c08f884
Nisa Delgado <nisadelgado@gmail.com>
2023-06-13 12:12:08 -0400

    Health: Uso de disco

--- packages/health/class/health.php
+++ packages/health/class/health.php
@@ -2,6 +2,18 @@
 
 class Health
 {
+	public function usedDiskSpace()
+	{
+		$diskTotalSpace = disk_total_space('/');
+		$diskFreeSpace = disk_free_space('/');
+
+		$percent = ($diskFreeSpace * 100) / $diskTotalSpace;
+		$percent = number_format($percent, 2);
+		$percent = $percent . '%';
+
+		return $percent;
+	}
+
 	public function view()
 	{
 		return view('health:index', compact('items'));
