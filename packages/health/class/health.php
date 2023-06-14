09b2f88e26ec0ab7dd16e5eaa5a3c5f8db737e3f
Nisa Delgado <nisadelgado@gmail.com>
2023-06-14 08:35:01 -0400

    Health: Ping

--- packages/health/class/health.php
+++ packages/health/class/health.php
@@ -2,6 +2,17 @@
 
 class Health
 {
+	public function ping($url)
+	{
+		exec("ping $url", $output);
+
+		if (trim($output[8]) == 'Paquetes: enviados = 4, recibidos = 4, perdidos = 0') {
+			return 'success';
+		}
+
+		return 'error';
+	}
+
 	public function usedDiskSpace()
 	{
 		$diskTotalSpace = disk_total_space('/');
