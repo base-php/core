fb4f8cce6513785321e35f77b60162e3806de73d
Nisa Delgado <nisadelgado@gmail.com>
2023-10-11 11:54:52 -0400

    Bing AI: Constructor, ask, withImage

--- support/class/bing-ai.php
+++ support/class/bing-ai.php
@@ -1,6 +1,33 @@
 <?php
 
+use MaximeRenou\BingAI\BingAI;
+use MaximeRenou\BingAI\Chat\Prompt;
+
 class BingAI
 {
-	
+	public $ai;
+	public $image;
+
+	public function __construct()
+	{
+		$this->ai = new BingAI(config('bing_cookie'));
+	}
+
+	public function ask($text)
+	{
+		$conversation = $this->ai->createChatConversation();
+		$prompt = new Prompt($text);
+
+		if ($this->withImage) {
+			$prompt->withImage($this->image);
+		}
+
+		return $conversation->ask($prompt);
+	}
+
+	public function withImage($path)
+	{
+		$this->image = $path;
+		return $this;
+	}
 }
