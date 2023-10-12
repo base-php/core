cd59f703b0ee3a31d6d695db4285451e276add62
Nisa Delgado <nisadelgado@gmail.com>
2023-10-12 12:13:21 -0400

    Bing AI: Create images, locale, tone

--- support/class/bing-ai.php
+++ support/class/bing-ai.php
@@ -2,11 +2,14 @@
 
 use MaximeRenou\BingAI\BingAI;
 use MaximeRenou\BingAI\Chat\Prompt;
+use MaximeRenou\BingAI\Chat\Tone;
 
 class BingAI
 {
 	public $ai;
+	public $country;
 	public $image;
+	public $language;
 
 	public function __construct()
 	{
@@ -16,16 +19,74 @@ class BingAI
 	public function ask($text)
 	{
 		$conversation = $this->ai->createChatConversation();
-		$prompt = new Prompt($text);
 
-		if ($this->withImage) {
-			$prompt->withImage($this->image);
+		if ($this->image) {
+			$conversation->withImage($this->image);
 		}
 
-		return $conversation->ask($prompt);
+		if ($this->language && $this->country) {
+			$combined = strtolower($this->language) . '-' . strtoupper($this->country);
+			$conversation->withPreferences($combined);
+		}
+
+		if ($this->tone) {
+			$conversation->withTone($this->tone);
+		}
+
+		return $conversation->ask(new Prompt($text));
 	}
 
-	public function withImage($path)
+	public function createImages($text)
+	{
+		$creator = $this->ai->createImages($text);
+
+		if ($this->image) {
+			$creator->withImage($this->image);
+		}
+
+		if ($this->language && $this->country) {
+			$combined = strtolower($this->language) . '-' . strtoupper($this->country);
+			$creator->withPreferences($combined);
+		}
+
+		if ($this->tone) {
+			$creator->withTone($this->tone);
+		}
+
+		$creator->wait();
+
+		if (! $creator->hasFailed()) {
+			return $creator->getImages();
+		}
+
+		return false;
+	}
+
+	public function locale($language, $country)
+	{
+		$this->language = $language;
+		$this->country = $country;
+		return $this;
+	}
+
+	public function tone($tone)
+	{
+		if ($tone == 'balanced') {
+			$this->tone = Tone::Balanced;
+		}
+
+		if ($tone == 'creative') {
+			$this->tone = Tone::Creative;
+		}
+
+		if ($tone == 'precise') {
+			$this->tone = Tone::Precise;
+		}
+
+		return $this;
+	}
+
+	public function image($path)
 	{
 		$this->image = $path;
 		return $this;
