e7e7052ce5bb5b08e4f5585032a0cd1da079ab4f
Nisa Delgado <nisadelgado@gmail.com>
2023-10-31 12:14:21 -0400

    Cash converter: Convert

--- packages/cash-converter/cash-converter.php
+++ packages/cash-converter/cash-converter.php
@@ -8,9 +8,17 @@ class CashConverter
 		$this->key = $config['exchange-rate-key'];
 	}
 
-	public function rate($form, $to)
+	public function convert($from, $to, $amount)
 	{
-		$http = http()->get('https://v6.exchangerate-api.com/v6/' . $this->key . '/latest/' . $form);
+		$http = http()->get('https://v6.exchangerate-api.com/v6/' . $this->key . '/latest/' . $from);
+		$body = $http->body();
+		$json = json($body);
+		return $amount * $json['conversion_rates'][$to];
+	}
+
+	public function rate($from, $to)
+	{
+		$http = http()->get('https://v6.exchangerate-api.com/v6/' . $this->key . '/latest/' . $from);
 		$body = $http->body();
 		$json = json($body);
 		return $json['conversion_rates'][$to];
