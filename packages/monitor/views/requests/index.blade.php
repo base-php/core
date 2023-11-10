86402effe5ea664d85f6a61a5d97382c1e92e8a2
Nisa Delgado <nisadelgado@gmail.com>
2023-11-10 10:08:30 -0400

    Monitor: Solicitudes en vistas

---
+++ packages/monitor/views/requests/index.blade.php
@@ -0,0 +1,46 @@
+<div class="shadow bg-white rounded">
+	<h1>Solicitudes</h1>
+
+	<table class="auto">
+		<thead>
+			<tr>
+				<th>#</th>
+				<th>Método</th>
+				<th>Ruta</th>
+				<th>Estado</th>
+				<th>Tiempo</th>
+				<th></th>
+			</tr>
+		</thead>
+
+		<tbody>
+			@foreach($requests as $request)
+				<tr>
+					<td>{{ $loop->iteration }}</td>
+
+					<td>
+						<span class="bg-neutral-300 p-3 rounded">
+							{{ $request->content['method'] }}
+						</span>
+					</td>
+
+					<td>{{ $request->content['path'] }}</td>
+
+					<td>
+						<span class="bg-neutral-300 p-3 rounded">
+							{{ $request->content['status'] }}
+						</span>
+					</td>
+
+					<td>{{ carbon()->create($request->content['time'])->diffForHumans() }}</td>
+
+					<td>
+						<a href="{{ '/request/' . $request->id }}">
+							<i class="fa fa-eye"></i>
+						</a>
+					</td>
+				</tr>
+			@endforeach
+		</tbody>
+	</table>
+</div>
---
+++ packages/monitor/views/requests/show.blade.php
@@ -0,0 +1,75 @@
+<div class="shadow bg-white rounded">
+	<h1>Detalles de la solicitud</h1>
+
+	<div>
+		<div>
+			<div class="w-1/4">Fecha y hora</div>
+			<div class="w-3/4">{{ $request->content['datetime'] }}</div>
+		</div>
+
+		<div>
+			<div class="w-1/4">Servidor</div>
+			<div class="w-3/4">{{ $request->content['hostname'] }}</div>
+		</div>
+
+		<div>
+			<div class="w-1/4">Método</div>
+			<div class="w-3/4">
+				<span class="bg-neutral-300 rounded p-3">
+					{{ $request->content['method'] }}
+				</span>
+			</div>
+		</div>
+
+		<div>
+			<div class="w-1/4">Ruta</div>
+			<div class="w-3/4">{{ $request->content['path'] }}</div>
+		</div>
+
+		<div>
+			<div class="w-1/4">Estado</div>
+			<div class="w-3/4">
+				<span class="bg-{{ $request->content['status'] == 200 ? 'green' : 'red' }}-300 p-3 rounded">
+					{{ $request->content['status'] }}
+				</span>
+			</div>
+		</div>
+
+		<div>
+			<div class="w-1/4">Duracíón</div>
+			<div class="w-3/4">{{ $request->content['duration'] }}</div>
+		</div>
+	</div>
+
+	<hr>
+
+	<div>
+		<h4>Cuerpo de la solicitud</h4>
+
+		<pre>
+			<code>
+				{{ $request->content['payload'] }}
+			</code>
+		</pre>
+	</div>
+
+	<div>
+		<h4>Encabezados</h4>
+
+		<pre>
+			<code>
+				{{ $request->content['headers'] }}
+			</code>
+		</pre>
+	</div>
+
+	<div>
+		<h4>Variables en sesión</h4>
+
+		<pre>
+			<code>
+				{{ $request->content['session'] }}
+			</code>
+		</pre>
+	</div>
+</div>
