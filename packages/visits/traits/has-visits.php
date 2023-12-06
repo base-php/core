78a63d04594367b55e60e2b8b2eec75a257e257d
Nisa Delgado <nisadelgado@gmail.com>
2023-12-06 07:22:47 -0400

    Visitas: Cantidad total de visitas, visitas, popular todo el tiempo

--- packages/visits/traits/has-visits.php
+++ packages/visits/traits/has-visits.php
@@ -8,6 +8,59 @@ trait HasVisits
 {
 	public $data;
 
+	public function data($items)
+	{
+		foreach ($items as $key => $value) {
+			$this->data[$key] = $value;
+		}
+
+		return $this;
+	}
+
+	public function ip()
+	{
+		$this->data['ip'] = $_SERVER['REMOTE_ADDR'];
+		return $this;
+	}
+
+	public function popularAllTime()
+	{
+		return DB::table('visits')
+			->select('COUNT(1) AS count')
+			->limit(10)
+			->orderBy('count')
+			->get();
+	}
+
+	public function popularToday()
+	{
+		return DB::table('visits')
+			->select('COUNT(1) AS count')
+			->whereDate('date_create', now('Y-m-d'))
+			->limit(10)
+			->orderBy('count')
+			->get();
+	}
+
+	public function session()
+	{
+		$this->data['session'] = $_SESSION;
+		return $this;
+	}
+
+	public function totalVisitCount()
+	{
+		return DB::table('visits')
+			->where('id_model', $this->id)
+			->count();
+	}
+
+	public function user($user)
+	{
+		$this->data['user'] = $user;
+		return $this;
+	}
+
 	public function visit()
 	{
 		$data = [
@@ -19,32 +72,5 @@ trait HasVisits
 		];
 
 		DB::table('visits')->insert($data);
-	}
-
-	public function withData($items)
-	{
-		foreach ($items as $key => $value) {
-			$this->data[$key] = $value;
-		}
-
-		return $this;
-	}
-
-	public function withIp()
-	{
-		$this->data['ip'] = $_SERVER['REMOTE_ADDR'];
-		return $this;
-	}
-
-	public function withSession()
-	{
-		$this->data['session'] = $_SESSION;
-		return $this;
-	}
-
-	public function withUser($user)
-	{
-		$this->data['user'] = $user;
-		return $this;
 	}
 }
