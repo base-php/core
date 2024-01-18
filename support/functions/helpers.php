9124bd1967cc8635258ef447aa8ff23b1c11afce
Nisa Delgado <nisadelgado@gmail.com>
2024-01-18 07:56:38 -0400

    Helper retry

--- support/functions/helpers.php
+++ support/functions/helpers.php
@@ -347,6 +347,23 @@ function response()
     return new Response();
 }
 
+function retry($callback, $attempts, $millisecondsBetweenAttempts)
+{
+    $success = false;
+    $attempt = 1;
+
+    while ($success == false && $attempt < $attempts) {
+        try {
+            $callback();
+            $success = true;
+
+        } catch (Exception $exception) {
+            $attempt++;
+            sleep($millisecondsBetweenAttemps);
+        }
+    }
+}
+
 function pdf($object)
 {
     return $object;
