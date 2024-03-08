6a5f3a083c3d4935cd72634ef29ca36ae999559a
Nisa Delgado <nisadelgado@gmail.com>
2024-03-08 08:14:40 -0400

    Método edit y refactorización de nueva clase CRUD

--- packages/crud/traits/crud.php
+++ packages/crud/traits/crud.php
@@ -11,17 +11,46 @@ trait CRUD
 	public function index()
 	{
 		$items = $this->model->get();
-		return view('crud:index', compact('items'));
+		return $this->view('index', compact('items'));
 	}
 
 	public function create()
 	{
-		return view('crud:create');
+		return $this->view('create');
 	}
 
 	public function store()
 	{
 		$this->model->create(request());
-		return redirect($route);
+		return redirect(plural());
+	}
+
+	public function edit($id)
+	{
+		$item = $this->model->find($id);
+		return $this->view('edit', compact('item'));
+	}
+
+	public function plural()
+	{
+		$class = get_class($this);
+		$class = str_replace('App\Controllers\\', '', $class);
+		$class = str_replace('Controller', '', $class);
+
+		$plural = str()
+			->to($class)
+			->plural()
+			->lower();
+
+		return $plural;
+	}
+
+	public function view($view, $data)
+	{
+		if (file_exists($_SERVER['DOCUMENT'] . '/resources/views/' . plural() . '/' . $view . '.blade.php')) {
+			return view(plural() . '.' . $view, $data);
+		}
+
+		return ('crud:' . $view, $data);
 	}
 }
