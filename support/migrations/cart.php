bc1a3f31ba0f2b9aae8cfc77d79a41b203c00f0e
Nisa Delgado <nisadelgado@gmail.com>
2024-04-29 12:15:48 -0400

    Migración para carrito de compra

---
+++ support/migrations/cart.php
@@ -0,0 +1,38 @@
+<?php
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        $this->schema->create('cart', function ($table) {
+            $table->id();
+
+            $table->string('id')
+                ->index();
+
+            $table->longText('data');
+
+            $table->datetime('date_create')
+                ->useCurrent();
+
+            $table->datetime('date_update')
+                ->useCurrent()
+                ->setCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        $this->schema->dropIfExists('cart');
+    }
+};
