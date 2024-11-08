af4b4cb2e1ff9fb0fb44372a19f6e577e81f12b1
Nisa Delgado <nisadelgado@gmail.com>
2024-11-08 08:50:42 -0400

    Comando para crear clase

---
+++ commands/commands/make-class.php
@@ -0,0 +1,62 @@
+<?php
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+
+#[AsCommand(name: 'make:class', description: 'Crea una nueva clase')]
+class MakeClass extends Command
+{
+    public function configure()
+    {
+        $this->addArgument('name', InputArgument::OPTIONAL);
+        $this->addOption('test', null, InputOption::VALUE_NONE, 'Genera una clase de prueba adjunta a la clase');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $name = $input->getArgument('name');
+
+        while (! $name) {
+            $question = new Question("\n- ¿Cuál es el nombre de la clase?\n> ");
+
+            $helper = $this->getHelper('question');
+            $name = $helper->ask($input, $output, $question);
+        }
+
+        $content = file_get_contents('vendor/base-php/core/commands/examples/class.php');
+        $content = str_replace('ClassName', $name, $content);
+
+        if (! file_exists('app/Class')) {
+            mkdir('app/Class');
+        }
+
+        $fopen = fopen('app/Class/'.$name.'.php', 'w+');
+        fwrite($fopen, $content);
+        fclose($fopen);
+
+        $style = new SymfonyStyle($input, $output);
+        $style->success("Clase '$name' creada satisfactoriamente.");
+
+        $test = $input->getOption('test');
+
+        if ($test) {
+            $content = file_get_contents('vendor/base-php/core/commands/examples/test.php');
+            $content = str_replace('TestName', $name, $content);
+
+            $fopen = fopen('tests/'.$name.'.php', 'w+');
+            fwrite($fopen, $content);
+            fclose($fopen);
+
+            $style = new SymfonyStyle($input, $output);
+            $style->success("Clase de prueba '$name' creada satisfactoriamente.");
+        }
+
+        return Command::SUCCESS;
+    }
+}
