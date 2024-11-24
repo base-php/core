d80da3b5be0b6f2f316e7442e6be0a5886ed9f99
Nisa Delgado <nisadelgado@gmail.com>
2024-11-24 16:33:10 -0400

    Comando para instalar desarrollo de escritorio

---
+++ commands/commands/desktop-install.php
@@ -0,0 +1,21 @@
+<?php
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Attribute\AsCommand;
+
+#[AsCommand(name: 'desktop:install', description: 'Instala todo los recursos para el desarrollo de escritorio')]
+class BillsTable extends Command
+{
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        system('npm install -g electron');
+
+        $style = new SymfonyStyle($input, $output);
+        $style->success('Desarrollo de escritorio instalado satisfactoriamente.');
+
+        return Command::SUCCESS;
+    }
+}
