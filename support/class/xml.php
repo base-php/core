2229c0fec0a281b100c2955020c90a80d6a46dd4
Nisa Delgado <nisadelgado@gmail.com>
2025-04-24 08:03:01 -0400

    Método para obtener un valor de clase XML

--- support/class/xml.php
+++ support/class/xml.php
@@ -8,8 +8,19 @@ class XML
     {
         if (file_exists($resource)) {
             $string = simplexml_load_file($resource);
-        } else {
+
+            if (! $string) {
+                return throw new Exception('File does not have a correct XML format.');
+            }
+        }
+
+        if (is_string($resource)) {
+            $resource = file_get_contents($resource);
             $string = simplexml_load_string($resource);
+
+            if (! $string) {
+                return throw new Exception('String does not have a correct XML format.');
+            }
         }
 
         $json = json_encode($string);
@@ -19,6 +30,11 @@ class XML
     public function all()
     {
         return (object) $this->data;
+    }
+
+    public function get($key)
+    {
+        return $this->data[$key] ?? null;
     }
 
     public function set($key, $value)
