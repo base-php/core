542db1ba3ffaa93b5176b64fe6119f728739b4af
nisa6delgado <nisadelgado@gmail.com>
2025-11-04 00:13:00 -0400

    Crear migración en módulo

--- commands/commands/module-make-migration.php
+++ commands/commands/module-make-migration.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Str;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
@@ -9,15 +10,14 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 
-#[AsCommand(name: 'module:make-command', description: 'Crea una nueva clase de comando en un módulo específico')]
-class ModuleMakeCommand extends Command
+#[AsCommand(name: 'module:make-migration', description: 'Crea un nuevo archivo de migración en módulo')]
+class ModuleMakeMigration extends Command
 {
     public function configure()
     {
         $this->addArgument('name', InputArgument::OPTIONAL);
         $this->addArgument('module', InputArgument::OPTIONAL);
-        $this->addOption('command', null, InputOption::VALUE_NONE, 'El comando de terminal al que será asignado [default: "command:name"]');
-        $this->addOption('test', null, InputOption::VALUE_NONE, 'Genera una clase de prueba adjunta al comando');
+        $this->addOption('path', null, InputOption::VALUE_REQUIRED, 'Ubicación donde el archivo de migración será creado');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -50,44 +50,31 @@ class ModuleMakeCommand extends Command
         $name = $input->getArgument('name');
 
         while (! $name) {
-            $question = new Question("\n- ¿Cuál es el nombre del comando?\n> ");
+            $question = new Question("\n- ¿Cuál es el nombre de la migración?\n> ");
 
             $helper = $this->getHelper('question');
             $name = $helper->ask($input, $output, $question);
         }
 
-        $content = file_get_contents('vendor/base-php/core/commands/examples/command.php');
-        $content = str_replace('CommandName', $name, $content);
-        $content = str_replace('App\Commands', 'Modules\\' . $module . '\Commands', $content);
+        $content = file_get_contents('vendor/base-php/core/commands/examples/Migration.php');
+        $content = str_replace('MigrationName', $name, $content);
 
-        $command = $input->getOption('command');
+        $var = Str::singular($name);
+        $var = strtolower($var);
+        $content = str_replace('VarName', $var, $content);
 
-        if ($command) {
-            $content = str_replace('public string $name = \'\';', 'public string $name = \'' . $name . '\';', $content);
-        }
+        $model = ucfirst(Str::singular($name));
+        $content = str_replace('ModelName', $model, $content);
 
-        if (! file_exists('modules/' . $module . '/Commands')) {
-            mkdir('modules/' . $module . '/Commands');
-        }
+        $name = date('Y_m_d_His') . '_' . $name;
 
-        $fopen = fopen('modules/' . $module . '/Commands/' . $name . '.php', 'w+');
+        $path = $input->getOption('path') ?? 'modules/' . $module . '/database';
+
+        $fopen = fopen($path . '/' . $name . '.php', 'w+');
         fwrite($fopen, $content);
         fclose($fopen);
-
-        $style->success("Clase de comando '$name' creada satisfactoriamente.");
-
-        $test = $input->getOption('test');
-
-        if ($test) {
-            $content = file_get_contents('vendor/base-php/core/commands/examples/test.php');
-            $content = str_replace('TestName', $name, $content);
-
-            $fopen = fopen('modules/' . $module . '/tests/' . $name . '.php', 'w+');
-            fwrite($fopen, $content);
-            fclose($fopen);
-            
-            $style->success("Clase de prueba '$name' creada satisfactoriamente.");
-        }
+        
+        $style->success("Archivo de migración '$name' creado satisfactoriamente.");
 
         return Command::SUCCESS;
     }
